16e40988610901fe8610a1022eb6927da1655f25
Tim Brice <tim.j.brice@gmailcom>
2020-08-07 13:44:27 -0500

    Make most methods in TTTGame private

--- lesson_5/tictactoe.rb
+++ lesson_5/tictactoe.rb
@@ -105,6 +105,29 @@ class TTTGame
     @current_marker = FIRST_TO_MOVE
   end
 
+  def play
+    clear
+    display_welcome_message
+
+    loop do
+      display_board
+
+      loop do
+        current_player_moves
+        break if board.someone_won? || board.full?
+        clear_screen_and_display_board if human_turn?
+      end
+      display_result
+      break unless play_again?
+      reset
+      display_play_again_message
+    end
+
+    display_goodbye_message
+  end
+
+  private
+
   def clear
     system('clear')
   end
@@ -194,27 +217,6 @@ class TTTGame
 
   def human_turn?
     @current_marker == HUMAN_MARKER
-  end
-
-  def play
-    clear
-    display_welcome_message
-
-    loop do
-      display_board
-
-      loop do
-        current_player_moves
-        break if board.someone_won? || board.full?
-        clear_screen_and_display_board if human_turn?
-      end
-      display_result
-      break unless play_again?
-      reset
-      display_play_again_message
-    end
-
-    display_goodbye_message
   end
 end
 
